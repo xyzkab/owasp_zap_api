e7f2c6bc381b49c5fceafee900101b6d867da850
xyzkab <xyzkab@l33tzapj>
2017-12-19 11:56:19 +0700

    add message method and dynamic @url

--- lib/core/message.rb
+++ lib/core/message.rb
@@ -2,6 +2,7 @@ module OwaspZapApi
   class Message
     attr_accessor :start,:count
     def initialize(params = {})
+      @id = params[:id]
       @start = params[:start]
       @count = params[:count]
       @format = params[:format] || 'JSON'
@@ -9,14 +10,20 @@ module OwaspZapApi
       @url = ZAPI + "#{@format}/core/"
     end
     def messages
-      @url += "view/messages"
+      url = @url + "view/messages"
       query = {:zapapiformat => @format,:baseurl => @baseurl,:start => @start,:count => @count}
-      Requester.get(@url,query)
+      Requester.get(url,query)
+    end
+    def message(id = nil)
+      @id ||= id
+      url = @url + "view/message"
+      query = {:zapapiformat => @format,:id => @id}
+      Requester.get(url,query)
     end
     def number
-      @url += "view/numberOfMessages/"
+      url = @url + "view/numberOfMessages/"
       query = {:zapapiformat => @format,:baseurl => @baseurl}
-      Requester.get(@url,query)
+      Requester.get(url,query)
     end
   end
 end
