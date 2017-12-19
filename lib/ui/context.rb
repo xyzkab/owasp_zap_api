ffabd35f0bfc7dfe3e2c0b93d83a30ac6d44115a
xyzkab <xyzkab@l33tzapj>
2017-12-19 13:53:21 +0700

    add create,export,include_in_context method

--- lib/ui/context.rb
+++ lib/ui/context.rb
@@ -3,6 +3,7 @@ module OwaspZapApi
     def initialize(params = {})
       @file = params[:file]
       @name = params[:name]
+      @regex = params[:regex]
       @format = params[:format] || 'JSON'
       @url = ZAPI + "#{@format}/context/"
     end
@@ -11,14 +12,34 @@ module OwaspZapApi
       query = {:zapapiformat => @format}
       Requester.get(url,query)
     end
-    def import
+    def create(name = nil)
+      @name ||= name
+      url = @url + "action/newContext"
+      query = {:zapapiformat => @format,:contextName => @name}
+    end
+    def import(file = nil)
+      @file ||= file
       url = @url + "action/importContext/"
       query = {:zapapiformat => @format,:contextFile => @file}
+      Requester.get(url,query)
+    end
+    def export(name = nil, file = nil)
+      @name ||= name
+      @file ||= file
+      url = @url + "action/exportContext"
+      query = {:zapapiformat => @format,:contextName => @name,:contextFile => @file}
       Requester.get(url,query)
     end
     def remove
       url = @url + "action/removeContext/"
       query = {:zapapiformat => @format,:contextName => @name}
+      Requester.get(url,query)
+    end
+    def include_in_context(name = nil,regex = nil)
+      @name ||= name
+      @regex ||= regex
+      url = @url + "action/includeInContext"
+      query = {:zapapiformat => @format,:contextName => @name,:regex => @regex}
       Requester.get(url,query)
     end
   end
