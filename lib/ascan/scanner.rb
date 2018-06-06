5073737093f23de5ef8cb4c8d62d9f4334b550ef
xyzkab <0xyzkab@gmail.com>
2018-06-06 13:20:27 +0700

    add ascan Scanner klass

---
+++ lib/ascan/scanner.rb
@@ -0,0 +1,22 @@
+module OwaspZapApi
+  class Scanner
+    def initialize(params = {})
+      @ids = params[:ids]
+      @format = params[:format] || 'JSON'
+      @url = OwaspZapApi.url + "#{@format}/ascan/"
+      @scan_policy = params[:scan_policy] || "Default Policy"
+    end
+
+    def disable_scanners
+      url = @url + "action/disableScanners"
+      query = {:zapapiformat => @format, :ids => @ids.join(","), :scanPolicyName => @scan_policy}
+      Requester.get(url,query)
+    end
+
+    def lists
+      url = @url + "view/scanners"
+      query = {:zapapiformat => @format, :scanPolicyName => @scan_policy}
+      Requester.get(url,query)
+    end
+  end
+end
